e7150b329068761a6e5fd1d4e712c45567a4e755
Rodrigo Mota <47394022+rmotadutra@users.noreply.github.com>
2021-08-26 15:30:21 -0300

    Creating the Covid Dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<Header />
+	<Menu @form="handleFormResult" />
+	<div class="container" v-if="data[0]">
+		<Chart
+			v-for="type in config.types"
+			:key="type.label"
+			class="chart"
+			:width="500"
+			:height="400"
+			:data="data"
+			:type="type"
+		/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+
+import Header from "./components/TheHeader.vue";
+import Menu from "./components/TheMenu.vue";
+import Chart from "./components/TheChart.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		Header,
+		Menu,
+		Chart,
+	},
+	data() {
+		return {
+			config: {},
+			data: [],
+			countries: [],
+		};
+	},
+	watch: {
+		"config.country": {
+			handler() {
+				this.fetchCovidData();
+			},
+		},
+	},
+	methods: {
+		async fetchCovidData() {
+			if (this.config.country) {
+				const url = `https://corona-api.com/countries/${this.config.country}?include=timeline`;
+				const response = (await axios(url)).data;
+				const data = response.data.timeline;
+				this.data.push({
+					country: this.config.country,
+					data,
+				});
+			}
+		},
+		handleFormResult(value) {
+			this.config = value;
+		},
+	},
+	mounted() {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+	}
+
+	.chart {
+		margin: 10px;
+	}
 </style>
