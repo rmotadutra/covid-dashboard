dd7dfb0e9690dea6a4e9f1175357b116afd362c8
Rodrigo Mota <47394022+rmotadutra@users.noreply.github.com>
2021-08-26 15:28:59 -0300

    Adding CheckBox component

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,47 @@
+<template>
+	<label :for="id" class="label">
+		<input
+			:id="id"
+			class="checkbox"
+			type="checkbox"
+			:checked="modelValue"
+			@input="$emit('update:modelValue', $event.target.checked)"
+		/>
+		{{ label }}
+	</label>
+</template>
+
+<script>
+export default {
+	props: {
+		id: {
+			type: String,
+		},
+		label: {
+			type: String,
+		},
+		modelValue: {
+			type: Boolean,
+		},
+	},
+};
+</script>
+
+<style>
+	.label {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		font-size: 1.2rem;
+		margin-right: 10px;
+		color: #1e1e1e;
+		cursor: pointer;
+		display: flex;
+		align-content: center;
+		align-items: center;
+	}
+
+	.checkbox {
+		zoom: 2;
+		margin: 2px;
+		cursor: pointer;
+	}
+</style>
